98e93713edc1048f7007a9918063058b3c45fc2c
Subas Shrestha <subasshrestha513@gmail.com>
2023-07-13 15:32:30 +0545

    Update contracts in typechain

--- packages/swap-contracts-typechain/typechain/factories/index.ts
+++ packages/swap-contracts-typechain/typechain/factories/index.ts
@@ -2,6 +2,7 @@
 /* tslint:disable */
 /* eslint-disable */
 export { FastPriceFeed__factory } from "./FastPriceFeed__factory";
+export { PositionManager__factory } from "./PositionManager__factory";
 export { PositionRouter__factory } from "./PositionRouter__factory";
 export { Vault__factory } from "./Vault__factory";
 export { VaultPriceFeed__factory } from "./VaultPriceFeed__factory";
